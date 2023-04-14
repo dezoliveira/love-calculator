c0a1dbb57f58f73c0184b86a9aaba618958da735
dezoliveira <andresoliveira@protonmail.com>
2023-04-14 16:56:37 -0300

    api endpoint bugfix

--- script.js
+++ script.js
@@ -1,82 +1,111 @@
 function calculate(){
   let personA = document.getElementById('personA').value
   let personB = document.getElementById('personB').value
+  let printMessage = document.getElementById('printMessage')
 
-  if(personA != "" || personB != ""){
+  if(personA != "" & personB != ""){
 
-    let requestOptions = {
+    const options = {
       method: 'GET',
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-      redirect: 'follow'    
+        'X-RapidAPI-Key': '6213ceb808msh5dc70b83ebce583p152098jsn891b5a6ce6c3',
+        'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
+      }
     };
 
-    fetch(`https://loverapi.herokuapp.com/api/v1/calculate?personA=${personA}&personB=${personB}`, requestOptions)
+    fetch(`https://love-calculator.p.rapidapi.com/getPercentage?fname=${personA}&sname=${personB}`, options)
 
-      .then((result) => result.text())
-      .then(result => JSON.parse(result))
+      .then((result) => result.json())
       .then(data => {
+        console.log(data)
         showMessage(data)
       })
-      .then(result => console.log(result))
       .catch(error => console.log('error', error));
   }
 
   else{
-
-    Swal.fire({
-      icon: 'error',
-      title: 'Opa!',
-      text: 'Você esqueceu de digitar os nomes! 😅😅😅',
-      confirmButtonText: 'Foi mal!'
-    })
-
+    callSwal(0)
   }
-  
 }
 
 function showMessage(data){
+  if(data){
+    let message = data.result
+    let personA = data.fname.toUpperCase()
+    let personB = data.sname.toUpperCase()
+    let percentage = data.percentage
 
-  let printMessage = document.getElementById('printMessage')
-  let message = data.message
-  
-  message = message.replace('and','e')
+    printMessage.classList.remove('hidden')
 
-  if (message.match('you both are perfect couple.')){
-    message = message.replace('you both are perfect couple.',
-                    'vocês formam um casal perfeito! 💘')
+    if (message.match('Congratulations! Good choice.')){
+      message = message.replace(
+        'Congratulations! Good choice.',
+        'Parabéns! Boa escolha! 💕'
+      )
+    }
+
+    if (message.match('All the best!')){
+      message = message.replace(
+        'All the best!',
+        'O melhor casal💘'
+      )
+    }
+
+    if (message.match('Can choose someone better.')){
+      message = message.replace(
+        'Can choose someone better.',
+        'Vocês devem escolher alguém melhor. 💔'
+      )
+    }
+
+    if (message.match('May be better next time.')){
+      message = message.replace(
+        'May be better next time.',
+        'Talvez na próxima reencarnação 😆'
+      )
+    }
+
+    printMessage.innerHTML = `
+      <div>
+        <h2>${personA} e ${personB}: </h4>
+        <span>
+          <p>${message}</p>
+          <p>Taxa de combinação: <strong>${percentage}%</strong></p>
+        </span>
+      </div>
+    `
+  } else {
+    callSwal(1)
   }
-
-  if (message.match('your relationship will be awesome carry on.')){
-    message = message.replace('your relationship will be awesome carry on.',
-                    'o relacionamento de vocês vai ser demais, continuem! 💕')
-  }
-
-  if (message.match('trust and care for each other everythings is gonna be ok.')){
-    message = message.replace('trust and care for each other everythings is gonna be ok.',
-                    'confiem e cuidem um do outro que tudo ficará bem. 💗')
-  }
-
-  if (message.match(`this isn't going to greate relationship maybe find someone else.`)){
-    message = message.replace(`this isn't going to greate relationship maybe find someone else.`,
-                    'esse não vai ser um bom relacionamento, talvez devessem encontrar outra pessoa. 💔')
-  }
-  
-  printMessage.innerHTML = `<p>${message}</p>`
-
+                            
   clearPersons()
 }
 
+function callSwal(messageType){
+  let swalText = ''
+  if(messageType === 0){
+    swalText = 'Você esqueceu de digitar os nomes! 😅😅😅'
+  }
+  else if(messageType === 1){
+    swalText = 'Ocorreu um erro com a API! 😅😅😅'
+  }
+
+  Swal.fire({
+    icon: 'error',
+    title: 'Opa!',
+    text: swalText,
+    confirmButtonText: 'Foi mal!'
+  })
+}
+
 function clearPersons(){
-  
-    let personA = document.getElementById("personA")
-    personA.value = ""
+  let personA = document.getElementById("personA")
+  personA.value = ""
 
-    let personB = document.getElementById("personB")
-    personB.value = ""
+  let personB = document.getElementById("personB")
+  personB.value = ""
 
-    personA.focus()
+  personA.focus()
 }
 
 let btnCalculate = document.getElementById('btnCalculate')
